d05f151e1ea83e9c20e4486ba6b288e282e2ae90
Reece1109 <reecesimon84@gmail.com>
2023-03-23 08:15:12 +0200

    Update

--- src/views/AdminPageView.vue
+++ src/views/AdminPageView.vue
@@ -2,6 +2,23 @@
   <div class="admin-page">
     <h1>Admin Page</h1>
     <img src="" alt="Airbnb Logo">
+    <div class="col-12 mt-5">
+      <h3>Admin Table</h3>
+      <button type="button" class="btn btn-info" data-bs-toggle="modal" data-target="#addAdmin">
+        ADD
+      </button>
+        <thead>
+          <tr>Name</tr>
+          <tr>Surname</tr>
+          <tr>Email</tr>
+          <tr>Cellphone Number</tr>
+          <tr>Role</tr>
+          <tr>Edit</tr>
+          <tr>Delete</tr>
+        </thead>
+
+        <tr v-for=" in this Admin"></tr>
+    </div>
   </div>
 </template>
 
